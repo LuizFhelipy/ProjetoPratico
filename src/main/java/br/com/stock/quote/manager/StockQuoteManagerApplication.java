cef82092e61d2dede0f6ff45dbe26a50f679d934
Luiz Fhelipy <fhelipy_rt@hotmail.com>
2021-05-21 11:21:57 -0300

    habilitando cache

--- src/main/java/br/com/stock/quote/manager/StockQuoteManagerApplication.java
+++ src/main/java/br/com/stock/quote/manager/StockQuoteManagerApplication.java
@@ -2,7 +2,9 @@ package br.com.stock.quote.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class StockQuoteManagerApplication {
 
